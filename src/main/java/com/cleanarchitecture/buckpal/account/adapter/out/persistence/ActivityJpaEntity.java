ffd50b62d176913763eceb23708145f98ac32b95
hocaron <kkannu0407@gmail.com>
2022-09-12 01:45:12 +0900

    feat: 영속성 어댑터 구현

---
+++ src/main/java/com/cleanarchitecture/buckpal/account/adapter/out/persistence/ActivityJpaEntity.java
@@ -0,0 +1,40 @@
+package com.cleanarchitecture.buckpal.account.adapter.out.persistence;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "activity")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+class ActivityJpaEntity {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column
+	private LocalDateTime timestamp;
+
+	@Column
+	private Long ownerAccountId;
+
+	@Column
+	private Long sourceAccountId;
+
+	@Column
+	private Long targetAccountId;
+
+	@Column
+	private Long amount;
+}
