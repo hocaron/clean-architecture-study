b4583f8736ef6ab61bf0007c773f763043dc901b
hocaron <kkannu0407@gmail.com>
2022-09-12 00:19:06 +0900

    feat: 비즈니스 로직 검증 프로퍼티 구현

---
+++ src/main/java/com/cleanarchitecture/buckpal/account/application/service/ThresholdExceededException.java
@@ -0,0 +1,10 @@
+package com.cleanarchitecture.buckpal.account.application.service;
+
+import com.cleanarchitecture.buckpal.account.domain.Money;
+
+public class ThresholdExceededException extends RuntimeException {
+
+	public ThresholdExceededException(Money threshold, Money actual) {
+		super(String.format("Maximum threshold for transferring money exceeded: tried to transfer %s but threshold is %s!", actual, threshold));
+	}
+}
